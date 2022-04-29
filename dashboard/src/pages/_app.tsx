4baed960fa12aabacb6507ba2ebda21615f88ec7
Marcel <marcelmiro1@hotmail.com>
2022-04-29 10:43:55 +0100

    Dashboard: add page transition progress bar

--- dashboard/src/pages/_app.tsx
+++ dashboard/src/pages/_app.tsx
@@ -1,10 +1,17 @@
-import '@/styles/globals.css'
+import '@/styles/globals.scss'
 import type { AppProps } from 'next/app'
+import NextNProgress from 'nextjs-progressbar'
 import Layout from '@/components/Layout'
 
 function MyApp({ Component, pageProps }: AppProps) {
 	return (
 		<Layout>
+			<NextNProgress
+				height={3}
+				showOnShallow={true}
+				options={{ showSpinner: false }}
+			/>
+
 			<Component {...pageProps} />
 		</Layout>
 	)
